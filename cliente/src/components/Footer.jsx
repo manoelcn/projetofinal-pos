d2d1cf7cdd0a5eb4eb007330e3d6db2655cb9073
manoel <candidomanoel78@gmail.com>
2024-12-13 15:38:05 -0300

    criando components comecando pelo footer

---
+++ cliente/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 text-center">
+            <div className="container">
+                <span className="text-muted">© 2024 StockSimplify</span>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
