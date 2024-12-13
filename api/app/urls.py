b967ab70ae3c8da0eec306ccc113fba477fbfba8
manoel <candidomanoel78@gmail.com>
2024-12-12 22:10:56 -0300

    add products urls ao arquivo principal

--- api/app/urls.py
+++ api/app/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('', include('brands.urls')),
     path('', include('categories.urls')),
     path('', include('suppliers.urls')),
+    path('', include('products.urls')),
 ]
