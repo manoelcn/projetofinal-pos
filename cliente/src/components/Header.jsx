4166b03415d03b122e9dbd7a202701e14bab28b4
manoel <candidomanoel78@gmail.com>
2024-12-13 15:40:15 -0300

    criando header component

---
+++ cliente/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+            <div className="container-fluid">
+                <a className="navbar-brand text-white" href="/home">StockSimplify</a>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
